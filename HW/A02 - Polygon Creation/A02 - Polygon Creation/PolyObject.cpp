22cf94d0e036e3981d9f50c3acd4db3e4fc6ac0c
Maddy Boussa <mboussa@nycap.rr.com>
2023-09-26 14:06:58 -0400

    set up A02

---
+++ HW/A02 - Polygon Creation/A02 - Polygon Creation/PolyObject.cpp
@@ -0,0 +1,30 @@
+#include "PolyObject.h"
+
+PolyObject::PolyObject()
+{
+}
+
+PolyObject::~PolyObject()
+{
+}
+
+void PolyObject::addVertex(vec2 p_vert)
+{
+}
+
+void PolyObject::setColor(vec3 p_color)
+{
+}
+
+unsigned int PolyObject::getVertNum()
+{
+	return 0;
+}
+
+void PolyObject::draw()
+{
+}
+
+void PolyObject::draw(vec2 p_mousePos)
+{
+}
